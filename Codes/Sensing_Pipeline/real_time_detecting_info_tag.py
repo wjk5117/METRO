ce0a0b602402d756a91f26d62d7c0d87f923406a
Jike Wang <jikewang@sjtu.edu.cn>
2023-10-09 10:54:32 +0800

    update the infor tag dection code

--- Codes/Sensing_Pipeline/real_time_detecting_info_tag.py
+++ Codes/Sensing_Pipeline/real_time_detecting_info_tag.py
@@ -1,15 +1,19 @@
 '''
-Only use x-axis data to detect the 0-degree passive magnet
+Only use the x-axis data to detect the information tag consisting of 0-degree magnet
 '''
-from scipy.signal import savgol_filter
 import multiprocessing as mp
-from can.bus import BusState
 import pandas as pd
+import numpy as np
+import datetime
 import binascii
 import serial
 import struct
+import time
 import can
 
+from can.bus import BusState
+from can.interfaces.pcan.basic import *
+from scipy.signal import savgol_filter
 from utils import *
 
 
@@ -46,11 +50,11 @@ no_x = 0  # total number of peaks
 n = 0  # index for the data points
 
 x, sx, dx, sdx = ([[] for _ in range(num)] for _ in range(4))
-slope_list_z, raw_list_z = ([[] for _ in range(num)] for _ in range(2))
-slope_thrd_z = ([10000] * num for _ in range(1))
-estimate_z = ([True] * num for _ in range(1))
-LastRAW_z = ([0] * num for _ in range(1))  # Record the time of last peak
-N_flag_z, S_flag_z = ([0] * num for _ in range(2))
+slope_list_x, raw_list_x = ([[] for _ in range(num)] for _ in range(2))
+slope_thrd_x = [10000] * num
+estimate_x = [True] * num
+LastRAW_x = [0] * num  # Record the index of the last peak
+N_flag_x, S_flag_x = ([0] * num for _ in range(2))
 
 SmoothVector_r = gaussian(np.arange(wnd + 1)[1:], wnd / 2, wnd / 2)
 SmoothVector_r = SmoothVector_r / (np.sum(SmoothVector_r))
@@ -62,14 +66,14 @@ data = bytearray(4 * (3 * num + 1))
 cnt = 1
 current = 0
 
-tag_z = []
+tag_x = []
 cnt_mag = 0
 fs = 350
 speed_list, angle_list = [], []
 
 
 # Get velocity and steering wheel angle data (SWA) from CAN Bus
-def readSpeed_SWA(listFrames, listFrames2):
+def read_Speed_SWA(listFrames, listFrames2):
     BUS = can.interface.Bus(bustype="pcan", channel="PCAN_USBBUS1", bitrate=500000)
     BUS.state = BusState.PASSIVE
     while True:
@@ -97,8 +101,8 @@ def detectMag(listFrames, listFrames2):
     arduino = serial.Serial(port=COM, baudrate=921600, timeout=None)
     arduino.flushInput()
     t = []
-    global cnt, current, no_x, no_z, n_x, n, cnt_mag, cnt_exp, x, z
-    global slope_thrd_x, slope_thrd_z, slope_list_x, slope_list_z, estimate_x, estimate_z, raw_result, raw_list_x, raw_list_z
+    global cnt, current, no_x, n_x, n, cnt_mag, cnt_exp, x, sx, dx, sdx
+    global slope_thrd_x, slope_list_x, estimate_x, raw_result, raw_list_x
     global speed_list, angle_list, delta_t
     try:
         while True:
@@ -120,146 +124,151 @@ def detectMag(listFrames, listFrames2):
                 interval, = struct.unpack('f', data[(12 * num):(4 + 12 * num)])
                 current += interval / 1000 / 1000
 
-                # buffer some data points before starting the detection
+                # buffer some x-axis data points before starting the detection
                 if n <= max(wnd, SG_wnd):
                     for i in range(num):
-                        sz[i].append(sensors[i, 2])
-                        dz[i].append(0)
-                        sdz[i].append(0)
+                        sx[i].append(sensors[i, 0])
+                        dx[i].append(0)
+                        sdx[i].append(0)
                     n += 1
                     continue
 
                 # Smoothing the raw data
                 for i in range(num):
-                    sz[i].append(np.sum(SmoothVector_r * (np.array(z[i][-wnd:]))))
+                    sx[i].append(np.sum(SmoothVector_r * (np.array(x[i][-wnd:]))))
 
                 # Savitzky–Golay filter for 1st derivative
                 for i in range(num):
-                    filtered = savgol_filter(sz[i][-SG_wnd:], SG_wnd, 1, deriv=1)
-                    dz[i].append(filtered[(SG_wnd - 1) // 2])  # avoid boundary effect
+                    filtered = savgol_filter(sx[i][-SG_wnd:], SG_wnd, 1, deriv=1)
+                    dx[i].append(filtered[(SG_wnd - 1) // 2])  # avoid boundary effect
 
                 # smooth the 1st derivative
                 for i in range(num):
-                    sdz[i].append(np.sum(SmoothVector_d * (np.array(dz[i][-wnd_d:]))))
+                    sdx[i].append(np.sum(SmoothVector_d * (np.array(dx[i][-wnd_d:]))))
 
                 for i in range(num):
-                    N_flag_z[i] = 0
-                    S_flag_z[i] = 0
-                    if len(slope_list_z[i]) == 50:
-                        if estimate_z[i]:
-                            slope_thrd_z[i] = amp_thrd_z[i] * np.abs(np.array(slope_list_z[i][1:])).max()
-                            print('Z axis of sensor %d: Pre-done with slope threshold equaling to %.2f' % (
-                                i + 1, slope_thrd_z[i]))
+                    N_flag_x[i] = 0
+                    S_flag_x[i] = 0
+                    if len(slope_list_x[i]) == 50:
+                        if estimate_x[i]:
+                            slope_thrd_x[i] = amp_thrd_x[i] * np.abs(np.array(slope_list_x[i][1:])).max()
+                            print('X axis of sensor %d: Pre-done with slope threshold equaling to %.2f' % (
+                                i + 1, slope_thrd_x[i]))
                             delta_time = (t[n] - t[n - 50]) / 50
                             print('Sampling rate: %.2f Hz' % (1 / delta_time))
-                            estimate_z[i] = False
-                            Last_z = str(datetime.datetime.now())
+                            estimate_x[i] = False
+                            Last_x = str(datetime.datetime.now())
 
                     # Detect the N polarity
-                    if sdz[i][-1] > 0 and sdz[i][-2] <= 0:
-                        slope_z = (sdz[i][-1] - sdz[i][-2])
-                        slope_list_z[i].append(slope_z)
-                        raw_z = sz[i][n - int((wnd + 1) / 2)]
-                        raw_list_z[i].append(raw_z)
-                        if slope_z >= slope_thrd_z[i]:
-                            if LastRAW_z[i] == 0:
-                                Now_z = str(datetime.datetime.now())
-                                t_interval_z = getInterval(Last_z, Now_z)
-                                if t_interval_z > delta_t:
-                                    N_flag_z[i] = 1
-                                    Last_z = Now_z
+                    if sdx[i][-1] > 0 and sdx[i][-2] <= 0:
+                        slope_x = (sdx[i][-1] - sdx[i][-2])
+                        slope_list_x[i].append(slope_x)
+                        raw_x = sx[i][n - int((wnd + 1) / 2)]
+                        raw_list_x[i].append(raw_x)
+                        if slope_x >= slope_thrd_x[i]:
+                            if LastRAW_x[i] == 0:
+                                Now_x = str(datetime.datetime.now())
+                                t_interval_x = getInterval(Last_x, Now_x)
+                                if t_interval_x > delta_t:
+                                    N_flag_x[i] = 1
+                                    Last_x = Now_x
                                 # no = no + 1
                                 # print('Sensor %d: No. %d detect a N' % (i+1, no))
 
                             else:
-                                if raw_z - LastRAW_z[i] <= -delta_thrd_z[i]:
-                                    Now_z = str(datetime.datetime.now())
-                                    t_interval_z = getInterval(Last_z, Now_z)
-                                    if t_interval_z > delta_t:
-                                        N_flag_z[i] = 1
-                                        Last_z = Now_z
+                                if raw_x - LastRAW_x[i] <= -delta_thrd_x[i]:
+                                    Now_x = str(datetime.datetime.now())
+                                    t_interval_x = getInterval(Last_x, Now_x)
+                                    if t_interval_x > delta_t:
+                                        N_flag_x[i] = 1
+                                        Last_x = Now_x
                                     # no = no + 1
                                     # print('Sensor %d: No. %d detect a N' % (i+1, no))
                         else:
-                            LastRAW_z[i] = raw_z
+                            LastRAW_x[i] = raw_x
 
                     # Detect the S polarity
-                    if sdz[i][-1] < 0 and sdz[i][-2] >= 0:
-                        slope_z = (sdz[i][-1] - sdz[i][-2])
-                        slope_list_z[i].append(slope_z)
-                        raw_z = sz[i][n - int((wnd + 1) / 2)]
-                        raw_list_z[i].append(raw_z)
-                        if slope_z <= -slope_thrd_z[i]:
-                            if LastRAW_z[i] == 0:
-                                Now_z = str(datetime.datetime.now())
-                                t_interval_z = getInterval(Last_z, Now_z)
-                                if t_interval_z > delta_t:
-                                    S_flag_z[i] = 1
-                                    Last_z = Now_z
+                    if sdx[i][-1] < 0 and sdx[i][-2] >= 0:
+                        slope_x = (sdx[i][-1] - sdx[i][-2])
+                        slope_list_x[i].append(slope_x)
+                        raw_x = sx[i][n - int((wnd + 1) / 2)]
+                        raw_list_x[i].append(raw_x)
+                        if slope_x <= -slope_thrd_x[i]:
+                            if LastRAW_x[i] == 0:
+                                Now_x = str(datetime.datetime.now())
+                                t_interval_x = getInterval(Last_x, Now_x)
+                                if t_interval_x > delta_t:
+                                    S_flag_x[i] = 1
+                                    Last_x = Now_x
                                 # no = no + 1
                                 # print('Sensor %d: No. %d detect a S' % (i+1, no))
                             else:
-                                if raw_z - LastRAW_z[i] >= delta_thrd_z[i]:
-                                    Now_z = str(datetime.datetime.now())
-                                    t_interval_z = getInterval(Last_z, Now_z)
-                                    if t_interval_z > delta_t:
-                                        S_flag_z[i] = 1
-                                        Last_z = Now_z
+                                if raw_x - LastRAW_x[i] >= delta_thrd_x[i]:
+                                    Now_x = str(datetime.datetime.now())
+                                    t_interval_x = getInterval(Last_x, Now_x)
+                                    if t_interval_x > delta_t:
+                                        S_flag_x[i] = 1
+                                        Last_x = Now_x
                                     # no = no + 1
                                     # print('Sensor %d: No. %d detect a S' % (i+1, no))
                         else:
-                            LastRAW_z[i] = raw_z
+                            LastRAW_x[i] = raw_x
 
                     file1 = open("results.txt", "a")
-                    if np.sum(np.array(N_flag_z)) > 0:
+                    if np.sum(np.array(N_flag_x)) > 0:
                         # record tag start
-                        no_z += 1
+                        no_x += 1
                         # speed, angle = listFrames[-1][1], listFrames2[-1][1]
                         print(str(datetime.datetime.now()) + ":", "speed is " + str(speed_list[-1][1]) + ";",
-                              "SWA is " + str(angle_list[-1][1]) + ";", 'z axis detects No. %d N;' % no_z,
+                              "SWA is " + str(angle_list[-1][1]) + ";", 'x axis detects No. %d N;' % no_x,
                               "peak index is " + str(cnt) + '\n')
                         file1.write(str(datetime.datetime.now()) + ": speed is " + str(
                             speed_list[-1][1]) + "; " + "SWA is " + str(
-                            angle_list[-1][1]) + "; z axis detects No." + str(no_z) + " N; " + "peak index is " + str(
+                            angle_list[-1][1]) + "; x axis detects No." + str(no_x) + " N; " + "peak index is " + str(
                             cnt) + '\n')
                         t1 = time.time() * 1000 * 1000
-                        tag_z.append([cnt_mag, cnt, "N", speed_list[-1][1], angle_list[-1][1]])
+                        tag_x.append([cnt_mag, cnt, "N", speed_list[-1][1], angle_list[-1][1]])
                         cnt_mag += 1
                         # Tag prototype consists of 3 magnets
                         if cnt_mag == 3:
-                            print("Tag info:", tag_z)
+                            print("Tag info:", tag_x)
                             # calculate the distance ratio of the tag
-                            calculate_ratio(tag_z, t1, delta_time, speed_list, angle_list)
+                            calculate_ratio(tag_x, t1, delta_time, speed_list, angle_list)
                             cnt_mag = 0
-                            tag_z.clear()
+                            tag_x.clear()
 
-                    if np.sum(np.array(S_flag_z)) > 0:
-                        no_z += 1
+                    if np.sum(np.array(S_flag_x)) > 0:
+                        no_x += 1
                         print(str(datetime.datetime.now()) + ":", "speed is " + str(speed_list[-1][1]) + ";",
-                              "SWA is " + str(angle_list[-1][1]), 'Z axis detects No. %d S polarity;' % no_z,
+                              "SWA is " + str(angle_list[-1][1]), 'x axis detects No. %d S polarity;' % no_x,
                               "peak index is " + str(cnt) + '\n')
                         file1.write(str(datetime.datetime.now()) + ": speed is " + str(
                             speed_list[-1][1]) + "; " + "SWA is " + str(
-                            angle_list[-1][1]) + "; z axis detects No." + str(no_z) + " S; " + "peak index is " + str(
+                            angle_list[-1][1]) + "; x axis detects No." + str(no_x) + " S; " + "peak index is " + str(
                             cnt) + '\n')
                         t1 = time.time() * 1000 * 1000
-                        tag_z.append([cnt_mag, cnt, "S", speed_list[-1][1], angle_list[-1][1]])
+                        tag_x.append([cnt_mag, cnt, "S", speed_list[-1][1], angle_list[-1][1]])
                         cnt_mag += 1
+                        # Tag prototype consists of 3 magnets
                         if cnt_mag == 3:
-                            print("Tag info:", tag_z)
-                            calculate_ratio(tag_z, t1, delta_time, speed_list, angle_list)
+                            print("Tag info:", tag_x)
+                            calculate_ratio(tag_x, t1, delta_time, speed_list, angle_list)
                             cnt_mag = 0
-                            tag_z.clear()
+                            tag_x.clear()
                 n = n + 1
             cnt += 1
 
     except KeyboardInterrupt:
+        # Record the data
         print("Output csv")
         test = pd.DataFrame(columns=raw_name, data=raw_result)
+        # Raw data
         test.to_csv("RawData_" + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + ".csv")
-        test_z = pd.DataFrame(columns=["THR_Z"], data=slope_thrd_z)
-        test_z.to_csv("THR_Z_" + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + ".csv")
+        test_x = pd.DataFrame(columns=["THR_X"], data=slope_thrd_x)
+        # Thresholds
+        test_x.to_csv("THR_X_" + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + ".csv")
 
+        # Raw speed and SWA data
         test_speed = pd.DataFrame(columns=["Time Stamp", "Speed"], data=speed_list)
         test_speed.to_csv(
             "RawSpeed_" + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + ".csv")
@@ -267,14 +276,15 @@ def detectMag(listFrames, listFrames2):
         test_SWA.to_csv(
             "RawSWA_" + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + ".csv")
 
+        # Slope and raw data of x-axis
         for i in range(num):
-            test_z = pd.DataFrame(columns=["Slope"], data=slope_list_z[i])
-            test2_z = pd.DataFrame(columns=["Raw data"], data=raw_list_z[i])
-            test_z.to_csv(
-                'Slope_Z_' + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + "_S" + str(
+            test_x = pd.DataFrame(columns=["Slope"], data=slope_list_x[i])
+            test2_x = pd.DataFrame(columns=["Raw data"], data=raw_list_x[i])
+            test_x.to_csv(
+                'Slope_X_' + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + "_S" + str(
                     i + 1) + '.csv')
-            test2_z.to_csv(
-                'Raw_Z_' + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + "_S" + str(
+            test2_x.to_csv(
+                'Raw_X_' + scenario + "_" + str(cur_speed) + "kmh_" + str(times) + "_S" + str(
                     i + 1) + '.csv')
         print("Exited")
 
@@ -283,7 +293,7 @@ def detectMag(listFrames, listFrames2):
 if __name__ == '__main__':
     shared_list = mp.Manager().list()
     shared_list_SWA = mp.Manager().list()
-    p1 = mp.Process(target=readSpeed_SWA, args=(shared_list, shared_list_SWA,))
+    p1 = mp.Process(target=read_Speed_SWA, args=(shared_list, shared_list_SWA,))
     p1.start()
     p2 = mp.Process(target=detectMag, args=(shared_list, shared_list_SWA,))
     p2.start()
